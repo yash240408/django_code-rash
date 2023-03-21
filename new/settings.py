98a3b092f454748b74febb927ccba1fe57ce7435
yash240408 <yash.mistry.g43@gmail.com>
2023-03-21 22:21:53 +0530

    Changes done

--- new/settings.py
+++ new/settings.py
@@ -71,15 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'new.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
